c87735393d25ba3c9d71d2303bbbc27d17546b5b
ntefera530 <nathan.tefera@outlook.com>
2026-03-08 22:19:03 -0700

    backend project setup and Spring config

---
+++ backend/rag-document-qna/src/main/java/com/tefera/rag_document_qna/RagDocumentQnaApplication.java
@@ -0,0 +1,13 @@
+package com.tefera.rag_document_qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagDocumentQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagDocumentQnaApplication.class, args);
+	}
+
+}
---
+++ backend/rag-document-qna/src/test/java/com/tefera/rag_document_qna/RagDocumentQnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tefera.rag_document_qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagDocumentQnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
